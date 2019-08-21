7362d40d27cd71e2c89700c99c201e32029ed973
Harsh <harsh.26020@gmail.com>
2019-08-21 11:22:13 +0530

    FIltered categories

--- parse.py
+++ parse.py
@@ -2,42 +2,63 @@ from xml.sax import parse, ContentHandler
 import nltk
 from collections import defaultdict
 import re
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 nltk.download('punkt')
 
 
-invertedIndex = defaultdict(lambda:defaultdict(int))
+# invertedIndex = defaultdict(lambda:defaultdict(int))
+invertedIndex = defaultdict(lambda:defaultdict(lambda:defaultdict(int)))
+docTitle = open("docTitle.txt","w") 
 
 # storing stopwords
-stopWords = set()
-try:
-	f = open('stop_words.txt', "r")
-	line = f.readline()
-	while line:
-		line = line.strip()
-		stopWords.add(line)
-		line = f.readline()
-	f.close()
-except:
-	print("Unable to open stopwords.txt")
+stopWords = set(stopwords.words('english'))
 
-# remove punctuations, dotall matches all characters including a new line
+# porter stemmer
+ps = nltk.stem.PorterStemmer()
+
 regEx = re.compile(r'[.,:;_\[\]{}()"/\']',re.DOTALL)
+regSym = re.compile(r"[~`!@#$%-^*+{\[}\]\|\\<>/?_\"]",re.DOTALL)
 
-def addToIndex(words, ID):
-	ps = nltk.stem.PorterStemmer()
+def addToIndex(words, ID, cur_type):
 	for word in words:
-		if word not in stopWords:
+		if len(word) >= 3 and word not in stopWords:
 			word = ps.stem(word)
-			invertedIndex[word][ID] += 1
-			# print(word, ID,invertedIndex[word][ID])
-
+			invertedIndex[word][ID][cur_type] += 1
+def reg_word(word):
+	word = regSym.sub(' ', word)
+	word = regEx.sub(' ', word)
+	return word
 # Takes a sentence as input and calls the addtoindex function
 def parse_sentence(word, ID, title, text):
-	word = regEx.sub(' ', word)
 	word = word.lower()
-	words = nltk.tokenize.word_tokenize(word)
 	if title:
-		addToIndex(words, ID)
+		word = regEx.sub(' ', word)
+		words = nltk.tokenize.word_tokenize(word)
+		addToIndex(words, ID, 't')
+	elif text:
+		# finding categories
+		categories = re.findall(r'\[\[category:(.*?)\]\]', word, flags=re.MULTILINE)
+		addToIndex(categories, ID, 'c')
+		
+		# finding references
+		references = re.findall(r'== ?references ?==(.*?)==', word, flags=re.DOTALL)
+		references = ' '.join(references)
+		references = regSym.sub(' ',references)
+		references = references.split()
+		addToIndex(references, ID, 'r')
+		
+		# content in infobox
+		infobox = re.findall(r'{{infobox(.*?)}}', word, flags=re.DOTALL)
+		infobox = ' '.join(infobox)
+		infobox = regSym.sub(' ',infobox)
+		infobox = infobox.split()
+		addToIndex(infobox, ID, 'i')
+		
+		# body of content
+		word = reg_word(word)
+		words = nltk.tokenize.word_tokenize(word)
+		addToIndex(infobox, ID, 'b')
 class WikipediaHandler(ContentHandler):
 	def __init__(self):
 		self.buffer = ""
@@ -61,9 +82,10 @@ class WikipediaHandler(ContentHandler):
 			self.idFlag = 1
 	def endElement(self, tag):
 		if self.titleFlag:
+			docTitle.write(str(self.ID) + "@" + self.buffer + "\n")
 			parse_sentence(self.buffer, self.ID, 1, 0)
 			self.titleFlag = 0
-			self.Title = self.buffer
+			self.title = self.buffer
 			self.buffer = ""
 		elif self.textFlag:
 			parse_sentence(self.buffer, self.ID, 0, 1)
@@ -75,4 +97,4 @@ class WikipediaHandler(ContentHandler):
 			self.idFlag = 0
 	def characters(self,content):
 		self.buffer = self.buffer + content
-parse("enwiki-latest-pages-articles26.xml-p42567204p42663461", WikipediaHandler())
+parse("test_data.xml", WikipediaHandler())
